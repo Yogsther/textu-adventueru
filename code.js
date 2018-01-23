81e84a6c67a145f3f2b48a2614c173ede6afaf77
Olle <yogsther@gmail.com>
2018-01-23 13:54:18 +0100

    Backup update

--- code.js
+++ code.js
@@ -7,46 +7,46 @@ var save = 0;
 var questsDisplayed = 0;
 var questIndex = save;
 
-function run(){
+function run() {
   /* Start the game. */
-  displayMessage(save)
+  displayMessage(save);
 }
 
-var questReadingSpeed = 1;
+var questReadingSpeed = 30;
 
-function choose(id){
+function choose(id) {
   var lastQuest = questIndex;
   questIndex = quests[questIndex].jumpto[id];
   displayMessage(questIndex);
   document.getElementById("choices_insert").innerHTML = "Choices: ";
   var displayElements = document.getElementsByClassName("display-message");
-  displayElements[displayElements.length-2].style.color = "grey";
+  displayElements[displayElements.length - 2].style.color = "grey";
 }
 
 
-function displayMessage(questID){
+function displayMessage(questID) {
   clearInterval(newInterval) // To be sure
   /* Quest ID is the index of the question displayed */
-  for(var i = 0; i < quests.length; i++){
-    if(questID == quests[i].index){
-      area.innerHTML += "<span id='display_" + questsDisplayed +"' class='display-message'></span>"
+  for (var i = 0; i < quests.length; i++) {
+    if (questID == quests[i].index) {
+      area.innerHTML += "<span id='display_" + questsDisplayed + "' class='display-message'></span>"
       window.writtenChars = 0;
       var choices = "<span style='color: #444444;'> Choices: ";
-      for(var l = 0; l < quests[i].options.length; l++){
+      for (var l = 0; l < quests[i].options.length; l++) {
         choices += "<a href='javascript:choose(" + l + ")'>[" + quests[i].options[l] + "]</a> ";
       }
       choices += "</span>";
       window.currentDisplayPhrase = quests[i].display.split("");
-      var newInterval = setInterval(function(){
-        if(currentDisplayPhrase[writtenChars] == undefined) {
+      var newInterval = setInterval(function() {
+        if (currentDisplayPhrase[writtenChars] == undefined) {
           document.getElementById("choices_insert").innerHTML = choices;
           clearInterval(newInterval);
           questsDisplayed++;
-          area.scrollTo(0,document.body.scrollHeight);
+          area.scrollTo(0, document.body.scrollHeight);
           return;
         };
         document.getElementById("display_" + questsDisplayed).innerHTML += currentDisplayPhrase[writtenChars];
-        area.scrollTo(0,document.body.scrollHeight);
+        area.scrollTo(0, document.body.scrollHeight);
         writtenChars++;
       }, questReadingSpeed);
       return;
@@ -55,7 +55,7 @@ function displayMessage(questID){
   console.warn("Quest ID: " + questID + " was not found!");
 }
 
-function convert(){
+function convert() {
   var input = document.getElementById("input");
   document.getElementById("output").value = JSON.stringify(input.value);
 }
--- quests.js
+++ quests.js
@@ -22,42 +22,51 @@ Template:
 
 */
 
-var quests = [
-  {
+var quests = [{
     index: 0,
     display: "Welcome to the Game, let's a go!",
     options: ["Start the game!"],
     jumpto: [1]
-  },{
+  }, {
     index: 1,
     display: "*You woke up*",
     options: ["Stand up", "Go back to sleep"],
     jumpto: [2, 1]
-  },{
+  }, {
     index: 2,
     display: "*You stood up*",
     options: ["Look around", "Go back to sleep", "Masturbate"],
     jumpto: [6, 1, 3]
-  },{
+  }, {
     index: 3,
     display: "It feels good",
     options: ["Stop it", "Get some help"],
     jumpto: [4, 5]
-  },{
+  }, {
     index: 6,
     display: "You see a knife",
     options: ["Grab the knife", "Keep looking"],
     jumpto: [7, 8]
-  },{
+  }, {
     index: 7,
     display: "What do you want to do with the knife?",
     options: ["Put it in your pocket", "Circumsize", "Kill yourself"],
     jumpto: [9, 10, 11]
-  },{
+  }, {
     index: 10,
-    display: "oof",
+    diszplay: "oof",
     options: ["..."],
-    jumpto: [10]
+    jumpto: [13]
+  }, {
+    index: 13,
+    display: "...",
+    options: ["..."],
+    jumpto: [13]
+  }, {
+    index: 11,
+    display: "You are dead, not big surprise",
+    options: ["Try again"],
+    jumpto: [0]
   }
 
 ]
