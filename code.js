19d32ec66e5946ed5b2936fda9ba24f2a771eb3c
Olle <yogsther@gmail.com>
2018-01-26 15:15:56 +0100

    Boi

--- code.js
+++ code.js
@@ -3,6 +3,9 @@
 var area = document.getElementById("area"); /* Text area */
 
 
+/* The rate of which the text is typed out. */
+var questReadingSpeed = 30;
+
 var save = 0;
 var questsDisplayed = 0;
 var questIndex = save;
@@ -20,35 +23,24 @@ function run() {
   displayMessage(save);
 }
 
-var questReadingSpeed = 30;
-
-function choose(id) {
-  /* This function called when the player chooses one of the options. */
-  var lastQuest = questIndex;
-  try {
-    quests[questIndex].special[questIndex]();
-  } catch (e) {}
-  questIndex = quests[questIndex].jumpto[id];
-
-  displayMessage(questIndex);
-  document.getElementById("choices_insert").innerHTML = "Choices: ";
-  var displayElements = document.getElementsByClassName("display-message");
-  displayElements[displayElements.length - 2].style.color = "grey";
-}
-
 
 function displayMessage(questID) {
   /* Display a new message on the screen */
   clearInterval(newInterval) // To be sure
+  addedChoices = []; // Clear extra choices
   /* Quest ID is the index of the question displayed */
+  var specialQueue = [];
   for (var i = 0; i < quests.length; i++) {
     // TODO: Implement getQuest instead of this mess.
     if (questID == quests[i].index) {
       area.innerHTML += "<span id='display_" + questsDisplayed + "' class='display-message'></span>"
       window.writtenChars = 0;
-      var choices = "<span style='color: #444444;'> Choices: ";
+      var choices = "<span id='choices-insert-final' style='color: #444444;'> Choices: ";
       for (var l = 0; l < quests[i].options.length; l++) {
         choices += "<a href='javascript:choose(" + l + ")'>[" + quests[i].options[l] + "]</a> ";
+        if(quests[i].run != undefined){
+          specialQueue.push(quests[i].run);
+        }
       }
       choices += "</span>";
       window.currentDisplayPhrase = quests[i].display.split("");
@@ -64,11 +56,98 @@ function displayMessage(questID) {
         area.scrollTo(0, document.body.scrollHeight);
         writtenChars++;
       }, questReadingSpeed);
+      for(var i = 0; i < specialQueue.length; i++){
+        console.log(specialQueue)
+        specialQueue[i]();
+      }
       return;
     }
   }
   console.warn("Quest ID: " + questID + " was not found!");
 }
+
+function choose(id) {
+  /* This function called when the player chooses one of the options. */
+  var lastQuest = questIndex;
+  try {
+    quests[questIndex].special[questIndex]();
+  } catch (e) {}
+  questIndex = quests[questIndex].jumpto[id];
+  displayMessage(questIndex);
+  document.getElementById("choices_insert").innerHTML = "Choices: ";
+  var displayElements = document.getElementsByClassName("display-message");
+  displayElements[displayElements.length - 2].style.color = "grey";
+}
+
+function chooseNew(id){
+  var choice = addedChoices[id];
+  if(choice.special !== null) choice.special();
+  displayMessage(choice.jumpto);
+  document.getElementById("choices_insert").innerHTML = "Choices: ";
+}
+
+var addedChoices = new Array();
+
+function addChoice(title, jumpto, special){
+  /*  Special does NOT have to be provided, Special is code that will run on click.
+      Add a choice to a quests, use this in the "run" option for quests.
+      Title is the choice title, jumpto is the jumpto-index.
+  */
+  if(special == undefined) special = null;
+  var id = addedChoices.length;
+  var choice = document.getElementById("choices-insert-final").innerHTML += "<a href='javascript:chooseNew(" + id + ")'>[" + title + "]</a>";
+  addedChoices[id] = {
+    jumpto: jumpto,
+    special: special
+  }
+}
+
+var overlayUp = false;
+function toggleOverlay(type){
+  /* For toggleing any overlay */
+  var overlayInsertSpan = document.getElementById("insert-overlay");
+  if(!overlayUp){
+    /* Add overlay */
+    overlayUp = true;
+    overlayInsertSpan.innerHTML = '<div id="overlay"></div>';
+    area.style.filter = "blur(2px)";
+
+  } else {
+    /* Remove overlay */
+    overlayUp = false;
+    overlayInsertSpan.innerHTML = '';
+    area.style.filter = "blur(0px)";
+    return;
+  }
+
+  window.overlayElement = document.getElementById("overlay");
+
+  if(type === "map"){
+    return;
+  }
+  if(type === "options"){
+    toggleOptions();
+    return;
+  }
+  if(type === "inventory")
+    return;
+}
+
+function toggleOptions(){
+  var optionsMenu = [{
+    title: "Toggle Darkmode",
+    run: "todo"
+  },{
+    title: "Toggle Developer Mode",
+    run: "todo"
+  }];
+  overlayElement.innerHTML = "<span id='options_overlay'></span>"
+  for(var i = 0; i < optionsMenu.length; i++){
+    document.getElementById("options_overlay").innerHTML += "<a class='option-item' href='javascript:" + optionsMenu[i].run + "'> [" + optionsMenu[i].title + "]</a><br>";
+  }
+
+}
+
 
 function giveItem(id) {
   var item = loadItem(id);
--- quests.js
+++ quests.js
@@ -11,6 +11,7 @@ you need as many jumpto's as options.
 Special | For a pure javascript function, this is usefull to give an Item or
 do something custom. Keep in mind if it's not the first option in the "options" you will need to
 insert a null before it so it will have the correct index.
+Run     | Run this code whenever this quests is displayed.
 
 
 Template:
@@ -20,14 +21,15 @@ Template:
   display: "TEXT",
   options: ["OPTION_1", "OPTION_2"],
   jumpto: [JUMPTO_1, JUMPTO_2],
-  special: [null, function(){ giveItem(23) // Any Javascript }]
+  special: [null, function(){ giveItem(23) // Any Javascript }],
+  run: function(){ // Run this code }
 }
 
 */
 
 var quests = [{
   index: 0,
-  display: "Welcome to Textu Adventuru (Working Title) v.0.2! Hit the \"Start Game\" choice to.. start the game!",
+  display: "Welcome to Textu Adventuru! Hit the \"Start Game\" choice to.. start the game!",
   options: ["Start Game", "... ?"],
   jumpto: [2, 1]
 },{
